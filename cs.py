ccb5ce75c991aa357b3b93cdf196e808db91cbdb
bernardogrodrigues <bernardo.rodrigues2005@gmail.com>
2024-02-24 14:43:37 +0000

    create cs.py (to better respect quanta format)

---
+++ cs.py
@@ -0,0 +1,12 @@
+"Candlestick stuff"
+
+class CandleStickItem:
+  """
+  Item to be plotted in a candlestick chart. I hope so
+
+  Attributes:
+    date (dict): Dictionary containing API key and interface instance.
+    time_series (dict): Dictionary containing main window components.
+  """
+  def _init_(self, date, time_series):
+    self
