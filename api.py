35ee0affac3585eb30cee37ba050cb623415ee66
bernardogrodrigues <bernardo.rodrigues2005@gmail.com>
2024-02-24 02:08:49 +0000

    Changed API function from TIME_SERIES_DAILY to FX_DAILY and changed symbol from NASDX to EURUSD.

--- api.py
+++ api.py
@@ -3,11 +3,15 @@
 import requests
 import json
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-#url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
-# H56D8PS4NZ92U0R5
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NASDX&interval=1min&apikey=H56D8PS4NZ92U0R5'
+fx_pair = "EURUSD"
+
+url = 'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&outputsize=full&apikey==H56D8PS4NZ92U0R5'
+
+apikey = "H56D8PS4NZ92U0R5"
 r = requests.get(url)
 data = r.json()
 
-print(json.dumps(data, indent=2))
+#print(json.dumps(data, indent=2))
+
+print(data)
+print(r)
