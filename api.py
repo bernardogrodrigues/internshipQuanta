b17f5f814bfc784061224e2c58fd39c0bed96d6b
bernardogrodrigues <bernardo.rodrigues2005@gmail.com>
2024-02-24 03:14:38 +0000

    started on api part, delivered a function that gets a time series in a dict form mainly from a forex pair and a timeframe

--- api.py
+++ api.py
@@ -3,15 +3,33 @@
 import requests
 import json
 
-fx_pair = "EURUSD"
+APIKEY = "H56D8PS4NZ92U0R5"
 
-url = 'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&outputsize=full&apikey==H56D8PS4NZ92U0R5'
+def getTimeSeries(fx_pair: str, time_frame: str = "daily", output: str = "compact") -> dict[str,dict]:
+  '''
+  Fetches a time series  of the given currency pair from the API
 
-apikey = "H56D8PS4NZ92U0R5"
-r = requests.get(url)
-data = r.json()
+  Parameters:
+    fx_pair (str): Currency pair to fetch data for.
+    time_frame (str): Timeframe of data (defaults to daily)
+    output (str): Output format (defaults to compact)
+  
+  Returns:
+    Dict: A dictionary with days as keys and ohlc data as values
+  '''
+  # Build URL
+  base_url = "https://www.alphavantage.co/query?"
+  # Linking function params to the  url parameters
+  tf = time_frame.upper()
+  from_currency = fx_pair.upper()[:3]
+  to_currency = fx_pair.upper()[-3:]
+  api_params = f"function=FX_{tf}&from_symbol={from_currency}&to_symbol={to_currency}&outputsize={output}&apikey=" + APIKEY
+  
+  # Requesting data from api
+  r = requests.get(base_url + api_params)
 
-#print(json.dumps(data, indent=2))
+  # delievering json data for requested time frame
+  return r.json()[f"Time Series FX ({tf[0]+tf[1:].lower()})"]
 
-print(data)
-print(r)
+# Used as confirmation that function is working correctly
+print(json.dumps(getTimeSeries("EURUSD"), indent=2))
